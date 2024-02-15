dde077e79e6bafc617511a8584488909024379f2
crnicavic <cavic5000@tutanota.com>
2024-02-15 23:52:22 +0100

    cleanup

--- blackjack/inc-MK.py
+++ blackjack/inc-MK.py
@@ -1,12 +1,41 @@
 from blackjack import *
 
 
-def calculate_gains(xp):
+def gatherxp(deck, policy, count=10):
+    def logs2xp(turnlog, result):
+        xp = []
+        #natural blackjack creates an empty log, nothing to learn from that
+        if not turnlog:
+            return xp
+        rewards = [0 for _ in turnlog]
+        rewards[-1] = result
+        for (hand, a), r in zip(turnlog, rewards):
+            xp.append((hand, a, r))
+        return xp
+
+    xp = []
+    wins = 0
+    score = 0
+    for _ in range(count):
+        result, plog, dlog = episode(deck, policy)
+        xp.append(logs2xp(plog, result)) 
+        wins += int(result == 1)
+        score += result
+    winrate = wins / count * 100      
+    score /= count
+    return xp, winrate, score
+
+
+#might be considered hacky, but i think it's elegant
+def calculate_gains(xp, gamma=1):
     G = np.zeros((22, 12, len(ACTIONS), 0)).tolist()
     for ep in xp:
-        for (state, a, g) in ep:
+        g = 0
+        for (state, a, r) in reversed(ep):
+            #r is 0 if not terminal
+            g += r 
             G[state.total][state.optotal][a].append(g)
-    
+            g *= gamma
     return G
 
 
@@ -41,11 +70,17 @@ def create_policy(Q, G, alpha=0.1):
 
     return policy
 
-deck = deck_init(20)
-Q = init_Q()
-xp = []
-for _ in range(40):
-    G = calculate_gains(xp)
-    player_pi = create_policy(Q, G)
-    xp, winrate, score = gatherxp(deck, player_pi, gamma=0.5, count=10000)
-    print(score)
+
+def inc_mk():
+    deck = deck_init(20)
+    Q = init_Q()
+    xp = []
+    best_score = -np.inf
+    best_policy = None
+    for _ in range(40):
+        G = calculate_gains(xp, gamma=0.9)
+        player_pi = create_policy(Q, G)
+        xp, winrate, score = gatherxp(deck, player_pi, count=10000)
+        if best_score < score:
+            best_policy = player_pi
+        print(score)
--- blackjack/naive-MK.py
+++ blackjack/naive-MK.py
@@ -1,12 +1,40 @@
 from blackjack import *
 
 
-def calculate_gains(xp):
+def gatherxp(deck, policy, count=10):
+    def logs2xp(turnlog, result):
+        xp = []
+        #natural blackjack creates an empty log, nothing to learn from that
+        if not turnlog:
+            return xp
+        rewards = [0 for _ in turnlog]
+        rewards[-1] = result
+        for (hand, a), r in zip(turnlog, rewards):
+            xp.append((hand, a, r))
+        return xp
+
+    xp = []
+    wins = 0
+    score = 0
+    for _ in range(count):
+        result, plog, dlog = episode(deck, policy)
+        xp.append(logs2xp(plog, result)) 
+        wins += int(result == 1)
+        score += result
+    winrate = wins / count * 100      
+    score /= count
+    return xp, winrate, score
+
+
+def calculate_gains(xp, gamma=1):
     G = np.zeros((22, 12, len(ACTIONS), 0)).tolist()
     for ep in xp:
-        for (state, a, g) in ep:
+        g = 0
+        for (state, a, r) in reversed(ep):
+            #r is 0 if not terminal
+            g += r 
             G[state.total][state.optotal][a].append(g)
-    
+            g *= gamma
     return G
 
 
@@ -27,10 +55,18 @@ def create_policy(G):
 
     return policy
 
-deck = deck_init(20)
-xp = []
-for _ in range(40):
-    G = calculate_gains(xp)
-    player_pi = create_policy(G)
-    xp, winrate, score = gatherxp(deck, player_pi, gamma=0.5, count=10000)
-    print(score)
+def naive_mk(maxiter=40, deckcount=9):
+    deck = deck_init(deckcount)
+    xp = []
+    best_score = -np.inf
+    best_policy = None
+    for _ in range(maxiter):
+        G = calculate_gains(xp)
+        player_pi = create_policy(G)
+        xp, winrate, score = gatherxp(deck, player_pi, count=10000)
+        if best_score < score:
+            best_policy = player_pi
+        print(score)
+    return best_policy
+
+naive_mk()
